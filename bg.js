82e4518bc61b06bf2b3a5ead65f532c5f0b8f710
unknown <qkrcksdna@naver.com>
2021-02-15 10:56:36 +0900

    배경

---
+++ bg.js
@@ -0,0 +1,27 @@
+const body = document.querySelector("body");
+
+const IMG_NUMBER = 4;
+function handleImgLoad(){
+    console.log("로딩 끝");
+}
+function paintImage(imgNumber){
+    const image = new Image();
+    image.src = `./images/${imgNumber}.jpg`;
+    image.onload = function(){
+        image.classList.add("bgImage");
+        body.appendChild(image);
+    }
+
+    // image.addEventListener("loadend", handleImgLoad);
+}
+
+function genRandomNumber(){
+    return Math.floor(Math.random()*IMG_NUMBER) + 1;
+}
+
+function init(){
+    const randNumb = genRandomNumber();
+    paintImage(randNumb);
+}
+
+init();
